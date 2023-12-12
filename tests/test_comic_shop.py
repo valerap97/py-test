2c69847203213254770a8bfb43ecf1e4ac0852eb
valerap97 <v.v.polikarpova.12.11.02@gmail.com>
2023-12-12 07:55:35 +0300

    More tests

--- tests/test_comic_shop.py
+++ tests/test_comic_shop.py
@@ -4,8 +4,8 @@ import sys
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, root_path)
-from exceptions import NegativeInventoryError
-from main import ComicBook
+from exceptions import NegativeInventoryError, DuplicateISBNError, ComicNotFoundError, ComicNotInStockError
+from main import ComicBook, ComicShop
 
 class TestComicBook(unittest.TestCase):
     
@@ -25,6 +25,55 @@ class TestComicBook(unittest.TestCase):
         comic = ComicBook("Batman", "Bob Kane", "12345", 9.99, 10)
         comic.update_inventory(15)
         self.assertEqual(comic.inventory_count, 15)
+        
+    def setUp(self):
+        self.shop = ComicShop("Comics R Us")
+
+        # Создаем несколько комиксов для тестирования
+        self.sample_comic = ComicBook("Superman", "Jerry Siegel", "12345", 5.99, 5)
+        self.sample_comic2 = ComicBook("Batman", "Bob Kane", "67890", 7.99, 5)
+
+        # Добавляем комикс в магазин
+        self.shop.add_comic(self.sample_comic)
+
+    def test_duplicate_isbn_error(self):
+        with self.assertRaises(DuplicateISBNError):
+            self.shop.add_comic(self.sample_comic)
+
+    def test_comic_not_found_by_title_error(self):
+        with self.assertRaises(ComicNotFoundError):
+            self.shop.get_comics_by_title("Nonexistent Title")
+
+    def test_comic_not_found_by_author_error(self):
+        with self.assertRaises(ComicNotFoundError):
+            self.shop.get_comics_by_author("Nonexistent Author")
+
+    def test_comic_not_found_by_isbn_error(self):
+        with self.assertRaises(ComicNotFoundError):
+            self.shop.get_comics_by_isbn("00000")
+
+    def test_not_in_stock_error(self):
+        with self.assertRaises(ComicNotInStockError):
+            self.sample_comic.inventory_count = 0
+            self.shop.sell_comic(self.sample_comic)
+
+    def test_remove_comic(self):
+        self.shop.remove_comic(self.sample_comic)
+        with self.assertRaises(ComicNotFoundError):
+            self.shop.get_comics_by_isbn(self.sample_comic.isbn)
+
+    def test_edit_comic_info_not_found_error(self):
+        with self.assertRaises(ComicNotFoundError):
+            self.shop.edit_comic_info(self.sample_comic2)
+            
+    def test_edit_comic_info(self):
+        new_title = "Superman: Earth One"
+        new_author = "J. Michael Straczynski"
+        new_price = 10.99
+        self.shop.edit_comic_info(self.sample_comic, title=new_title, author=new_author, price=new_price)
+        self.assertEqual(self.sample_comic.title, new_title)
+        self.assertEqual(self.sample_comic.author, new_author)
+        self.assertEqual(self.sample_comic.price, new_price)
 
 # запустить тесты
 if __name__ == '__main__':
--- tests/test_discount_manager.py
+++ tests/test_discount_manager.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import sys
+import io
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, root_path)
@@ -26,6 +27,31 @@ class TestComicShopIntegration(unittest.TestCase):
         self.discount_manager.define_discount("88888", 10)
         updated_comic = self.shop.get_comics_by_isbn("88888")[0]
         self.assertAlmostEqual(updated_comic.price, 11.69, places=2)
+        
+    def test_apply_discount_to_nonexistent_isbn(self):
+        with self.assertRaises(ComicNotFoundError):
+            self.discount_manager.apply_discount_to_isbn("99999", 10)
+
+    def test_define_invalid_discount(self):
+        with self.assertRaises(InvalidDiscountError):
+            self.discount_manager.define_discount("88888", -10)  # Недопустимый процент скидки
+        with self.assertRaises(InvalidDiscountError):
+            self.discount_manager.define_discount("88888", 150)  # Превышение 100%
+
+    def test_display_no_discounts(self):
+        # Предполагается, что в DiscountManager нет скидок,
+        # можно очистить словарь для уверенности.
+        self.discount_manager.discounts = {}
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.discount_manager.display_discounts()
+            self.assertEqual(mock_stdout.getvalue().strip(), "Скидок нет!")
+
+    def test_display_discounts(self):
+        # Прежде чем проверить вывод, необходимо обеспечить, что скидки уже определены.
+        self.discount_manager.define_discount("88888", 10)
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.discount_manager.display_discounts()
+            self.assertIn("Комикс 'Spider-Man': 10% скидка.", mock_stdout.getvalue().strip())
 
 # запустить тесты
 if __name__ == '__main__':
